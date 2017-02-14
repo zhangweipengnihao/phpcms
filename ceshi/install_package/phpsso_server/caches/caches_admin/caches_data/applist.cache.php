d78fc0872f9c5032cef1c6f85c39128384dbb044
张伟鹏 <1131373886@qq.com>
2017-02-14 20:39:48 +0800

    phpcms:init

---
+++ ceshi/install_package/phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://127.0.0.1/pcms/phpcms/ceshi/install_package/',
+    'authkey' => '94qUnOKFQo2fwg8PkxNx09h4Yn1VerGR',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
